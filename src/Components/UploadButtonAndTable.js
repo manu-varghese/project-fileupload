52faa8879a64f5c73786ad556dc85406810b071d
manu varghese <manu@inplass.co>
2023-07-13 14:00:58 +0530

    component changed

--- src/Components/UploadButtonAndTable.js
+++ src/Components/UploadButtonAndTable.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Table, Upload } from 'antd';
+import { Button, Modal, Form, Input, Table, Upload } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
 import { db } from '../Config/firebase';
 import { collection, getDocs } from 'firebase/firestore';
@@ -7,6 +7,7 @@ import { collection, getDocs } from 'firebase/firestore';
 const UploadButtonAndTable = () => {
   const [fileList, setFileList] = useState([]);
   const [tableData, setTableData] = useState([]);
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -26,13 +27,28 @@ const UploadButtonAndTable = () => {
   const handleFileUpload = (event) => {
     const { fileList } = event;
     setFileList(fileList);
+    setIsModalVisible(true);
   };
 
-  const columns = tableData.length > 0 ? Object.keys(tableData[0]).map((key) => ({
-    title: key,
-    dataIndex: key,
-    key,
-  })) : [];
+  const handleCancel = () => {
+    setFileList([]);
+    setIsModalVisible(false);
+  };
+
+  const handleFormSubmit = (values) => {
+    console.log('Form values:', values);
+    // Perform necessary actions with the form values
+    setIsModalVisible(false);
+  };
+
+  const columns =
+    tableData.length > 0
+      ? Object.keys(tableData[0]).map((key) => ({
+          title: key,
+          dataIndex: key,
+          key,
+        }))
+      : [];
 
   return (
     <div>
@@ -43,6 +59,45 @@ const UploadButtonAndTable = () => {
       </Upload>
 
       <Table dataSource={tableData} columns={columns} />
+
+      <Modal
+        title="Upload Form"
+        visible={isModalVisible}
+        onCancel={handleCancel}
+        footer={null}
+      >
+        <Form onFinish={handleFormSubmit}>
+          <Form.Item
+            label="Field 1"
+            name="field1"
+            rules={[{ required: true, message: 'Field 1 is required' }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Field 2"
+            name="field2"
+            rules={[{ required: true, message: 'Field 2 is required' }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Field 3"
+            name="field3"
+            rules={[{ required: true, message: 'Field 3 is required' }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
     </div>
   );
 };
